7188ad1b0b4ddf84fb535a2dd025eaf54f3c0e2e
tomikmar <tomikmar@users.noreply.github.com>
2020-07-19 22:09:38 +0200

    Update prepareBackupFile.sh

--- scripts/prepareBackupFile.sh
+++ scripts/prepareBackupFile.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
+# Key generation
+#   gpg [--expert] --full-generate-key
+#   gpg --list-keys
+#   gpg --list-secret-keys
+# Key export
+#   gpg --export -a "backup-test" > backup-test-public.key
+#   gpg --export-secret-key -a "backup-test" > backup-test-private.key
+# Key import
+#   gpg --allow-secret-key-import --import backup-test-private.key
+#   gpg --import backup-test-public.key
+
+
 [ $# -eq 0 ] && { echo "Please provide directory to backup as a first argument."; exit 1; }
 DIR_TO_BACKUP=$1
 [ ! -e $DIR_TO_BACKUP ] && { echo "Directory \"$DIR_TO_BACKUP\" does not exist."; exit 1; }
@@ -13,4 +26,8 @@ echo "Generating content summary ..."
 cd $DIR_TO_BACKUP
 bash createContentInfo.sh
 cd ..
+
+
+#gpg -e -r "backup-test" myfile.txt
+#gpg -d file.txt.gpg > file.txt
 
