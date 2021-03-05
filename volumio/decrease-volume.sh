1e5e918655cf15de1a3c3220c90abe5a31f153f3
tomikmar <tomikmar@users.noreply.github.com>
2021-03-05 23:27:04 +0100

    Volumio: use common.sh

--- volumio/decrease-volume.sh
+++ volumio/decrease-volume.sh
@@ -3,16 +3,18 @@
 [ -z "$1" ] && echo "Provide requested volume as the first parameter (0-100)" && exit 1
 [ ! -z "${1##[0-9]*}" ] && echo "First parameter must be a number (0-100)" && exit 2
 
+source common.sh
+
 REQUESTED_VOLUME=$1
 CURRENT_VOLUME=`volumio volume`
 
 
 if [ "$CURRENT_VOLUME" -gt "$REQUESTED_VOLUME" ]; then
-       	echo `date +"%Y-%m-%d-%H:%M:%S:"` "Decreasing volume from $CURRENT_VOLUME to $REQUESTED_VOLUME."
+       	log "Decreasing volume from $CURRENT_VOLUME to $REQUESTED_VOLUME."
 	[ -f /opt/volumio-scripts/illuminate.sh ] && /opt/volumio-scripts/illuminate.sh 10
 	volumio volume $REQUESTED_VOLUME
 	echo
 else
-       	echo `date +"%Y-%m-%d-%H:%M:%S:"` "Current volume $CURRENT_VOLUME is already below requested value $REQUESTED_VOLUME. Ignoring."
+       	log "Current volume $CURRENT_VOLUME is already below or equal to requested value $REQUESTED_VOLUME. Ignoring."
 fi
 
