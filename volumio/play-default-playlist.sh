666902e9de1e76bc4ebf41d6eb434ebbe7f2497c
tomikmar <tomikmar@users.noreply.github.com>
2021-03-07 16:29:08 +0100

    Volumio: fix links

--- volumio/play-default-playlist.sh
+++ volumio/play-default-playlist.sh
@@ -9,10 +9,10 @@ source /opt/volumio-scripts/common.sh
 PLAYLIST_MORNING="default-morning.json"
 PLAYLIST_AFTERNOON="default-morning.json"
 PLAYLIST_EVENING="default-evening.json"
-VOLUMIO_BASE_URL=http://localhost:3000/api/v1/getState
+VOLUMIO_BASE_URL=http://localhost:3000/api/v1
 
 
-until $(curl --silent --output /dev/null --head --fail ${VOLUMIO_BASE_URL}); do
+until $(curl --silent --output /dev/null --head --fail ${VOLUMIO_BASE_URL}/getState); do
     logAndSay "Waiting till Volumio is up and running ..."
     sleep 10s
 done
@@ -29,15 +29,15 @@ HOUR=`date +%H`
 if [ $HOUR -lt 11 ]; then
     log "Starting morning playlist $PLAYLIST_MORNING ..."
     volumio volume 40
-    curl "${VOLUMIO_BASE_URL}/?cmd=playplaylist&name=$PLAYLIST_MORNING"
+    curl "${VOLUMIO_BASE_URL}/commands/?cmd=playplaylist&name=$PLAYLIST_MORNING"
 elif [ $HOUR -lt 19 ]; then
     log "Starting afternoon playlist $PLAYLIST_MORNING ..."
     volumio volume 40
-    curl "${VOLUMIO_BASE_URL}/?cmd=playplaylist&name=$PLAYLIST_MORNING"
+    curl "${VOLUMIO_BASE_URL}/commands/?cmd=playplaylist&name=$PLAYLIST_MORNING"
 else
     log "Starting evening playlist $PLAYLIST_EVENING ..."
     volumio volume 30
-    curl "${VOLUMIO_BASE_URL}/?cmd=playplaylist&name=$PLAYLIST_EVENING"
+    curl "${VOLUMIO_BASE_URL}/commands/?cmd=playplaylist&name=$PLAYLIST_EVENING"
 fi
 echo
 
