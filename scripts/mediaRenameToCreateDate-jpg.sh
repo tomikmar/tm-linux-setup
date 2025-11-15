ff492fdf96b94fe4fa60dc57ceccb99559e6a856
tomikmar <tomikmar@users.noreply.github.com>
2025-11-15 13:41:53 +0100

    media rename: improve case for jpg

--- scripts/mediaRenameToCreateDate-jpg.sh
+++ scripts/mediaRenameToCreateDate-jpg.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 
-exiftool -fileOrder DateTimeOriginal -if 'not $XMP-xmpMM:PreservedFileName' '-XMP-xmpMM:PreservedFileName<$FileName' -overwrite_original -preserve -extension JPG *.JPG
+exiftool -fileOrder DateTimeOriginal -if 'not $XMP-xmpMM:PreservedFileName' '-XMP-xmpMM:PreservedFileName<$FileName' -overwrite_original -preserve -extension jpg -ignoreCase .
 
-exiftool -fileOrder DateTimeOriginal '-FileName<DateTimeOriginal' -d '%Y-%m-%d_%H%M%S%%-3c.%%le' -extension JPG *.JPG
+exiftool -fileOrder DateTimeOriginal '-FileName<DateTimeOriginal' -d '%Y-%m-%d_%H%M%S%%-3c.%%le' -extension jpg -ignoreCase .
 
